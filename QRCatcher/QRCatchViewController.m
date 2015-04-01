378e44699e0cd8af6b1db509e5bb9d1269c3c6d5
100mango <100mango@users.noreply.github.com>
2015-04-01 20:00:38 +0800

    完成二维码解析

---
+++ QRCatcher/QRCatchViewController.m
@@ -0,0 +1,72 @@
+//
+//  ViewController.m
+//  QRCatcher
+//
+//  Created by Mango on 15/4/1.
+//  Copyright (c) 2015年 Mango. All rights reserved.
+//
+
+#import "QRCatchViewController.h"
+@import AVFoundation;
+
+@interface QRCatchViewController ()<AVCaptureMetadataOutputObjectsDelegate>
+
+@end
+
+@implementation QRCatchViewController
+
+#pragma mark View Life Cycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //session
+    AVCaptureSession *session = [[AVCaptureSession alloc] init];
+    //device
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    NSError *error = nil;
+    //input
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:device error:&error];
+    if(input) {
+        [session addInput:input];
+    } else {
+        NSLog(@"%@", error);
+        return;
+    }
+    //output
+    AVCaptureMetadataOutput *output = [[AVCaptureMetadataOutput alloc] init];
+    [output setMetadataObjectTypes:@[AVMetadataObjectTypeQRCode]];
+    [output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    [session addOutput:output];
+    
+    //add preview layer
+    AVCaptureVideoPreviewLayer *previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:session];
+    previewLayer.frame = self.view.bounds;
+    [self.view.layer addSublayer:previewLayer];
+    
+    //start
+    [session startRunning];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+#pragma mark - AVCaptureMetadataOutputObjectsDelegate
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection
+{
+    for (AVMetadataMachineReadableCodeObject *metadata in metadataObjects) {
+        if ([metadata.type isEqualToString:AVMetadataObjectTypeQRCode]) {
+            
+            NSLog(@"%@",metadata.stringValue);
+            
+        }
+    }
+}
+
+
+@end
