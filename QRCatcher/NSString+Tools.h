7ea27f40ec053f2dd7fba3adf23a23dd88e2c463
100mango <100mango@users.noreply.github.com>
2015-04-01 20:13:45 +0800

    add tools

---
+++ QRCatcher/NSString+Tools.h
@@ -0,0 +1,21 @@
+//
+//  NSString+Tools.h
+//  Trans
+//
+//  Created by Mango on 15/2/2.
+//  Copyright (c) 2015年 Mango. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Tools)
+
+- (BOOL)isMobileNumber:(NSString *)mobileNum;
+
+//只检查车牌号 （不包括缩写与英文代号：例如：京A)
+- (BOOL)isCarNumber;
+
+//检查是否字符串为网址
+- (BOOL)isURL;
+
+@end
