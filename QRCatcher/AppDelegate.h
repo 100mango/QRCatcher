5807e25f38d24d1cd6e53384ac9b3b1bf613e4a5
100mango <100mango@users.noreply.github.com>
2015-04-07 16:04:06 +0800

    add core data

--- QRCatcher/AppDelegate.h
+++ QRCatcher/AppDelegate.h
@@ -7,11 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
++ (AppDelegate *)appDelegate;
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
 
 @end
 
