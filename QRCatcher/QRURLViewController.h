01164d5ca5212cc7ba4cd9c483cf9a82a4bf0704
100mango <100mango@users.noreply.github.com>
2015-04-07 17:10:41 +0800

    add url view controller

---
+++ QRCatcher/QRURLViewController.h
@@ -0,0 +1,13 @@
+//
+//  QRURLViewController.h
+//  QRCatcher
+//
+//  Created by Mango on 15/4/7.
+//  Copyright (c) 2015年 Mango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QRURLViewController : UIViewController
+
+@end
